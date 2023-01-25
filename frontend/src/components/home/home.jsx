e8da9f5516c184ff5315d31838ade35aa50322c0
HenriqueHagio <heryque08@gmail.com>
2023-01-25 17:16:07 -0500

    fim

---
+++ frontend/src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/main";
+
+export default props =>
+    <Main icon='home' title='inicio'
+        subtitle="segundo projeto do capitulo de react">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p>Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+    </Main>
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import { HashRouter } from 'react-router-dom'
+import './app.css'
+import React from 'react'
+import Routes from './routes'
+import Logo from '../components/template/logo'
+import Nav from '../components/template/nav'
+import Footer from '../components/template/footer'
+
+export default props =>
+      <HashRouter>
+            <div className='app'>
+                  <Logo />
+                  <Nav />
+                  <Routes />
+                  <Footer />
+            </div>
+      </HashRouter>
+
